8bbe90f17a57d8411bad7a23039a45ca98962728
bacn <mbachmann@trdi.io>
2020-08-11 20:54:54 +0200

    initial create database

---
+++ Scripts/course-management.sql
@@ -0,0 +1,8 @@
+
+DROP SCHEMA IF EXISTS `course-management`;
+-- In MySQL, the schema is the synonym for the database
+-- DROP DATABASE [IF EXISTS] database_name;
+
+CREATE SCHEMA `course-management` DEFAULT CHARACTER SET utf8 ;
+USE `course-management`;
+
