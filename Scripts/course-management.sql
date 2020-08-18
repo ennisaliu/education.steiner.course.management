ceb0a11124e49cf9e1dca3c70ecb8bb4ef0794e2
Ennis <66329667+ennisaliu@users.noreply.github.com>
2020-08-18 20:26:14 +0200

    db creation & data insert

--- Scripts/course-management.sql
+++ Scripts/course-management.sql
@@ -41,11 +41,11 @@ ALTER TABLE `course-management`.person ADD CONSTRAINT Personen_FK FOREIGN KEY (f
 INSERT INTO `course-management`.person
 (pnr, name, vorname, fnr, lohnstufe)
 VALUES(100001, 'Steffen', 'Felix', 3, 5),
-(232452, 'Müller', 'Hugo', 1, 1),
+(232452, 'MÃ¼ller', 'Hugo', 1, 1),
 (334643, 'Meier', 'Hans', 2, 5),
 (567231, 'Schmid', 'Beat', 3, 4),
-(345727, 'Steiner', 'René', 5, 5),
-(233456, 'Müller', 'Franz', 4, 7),
+(345727, 'Steiner', 'RenÃ©', 5, 5),
+(233456, 'MÃ¼ller', 'Franz', 4, 7),
 (132442, 'Osswald', 'Kurt', 1, 2),
 (345678, 'Metzger', 'Paul', 1, 1),
 (344556, 'Scherrer', 'Daniel', 2, 4),
@@ -75,15 +75,40 @@ VALUES(100001, 'Steffen', 'Felix', 3, 5),
 
 -- kurskontrolle (fnr, knr) Ennis
 
+
 CREATE TABLE `Kurskontrolle` (
-  `pnr` int NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `vorname` varchar(100) DEFAULT NULL,
-  `fnr` int DEFAULT NULL,
-  `lohnstufe` varchar(100) DEFAULT NULL,
-    PRIMARY KEY (`pnr`)
+  `FNr` int NOT NULL,
+  `KNr` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-ALTER TABLE `course-management`.person ADD CONSTRAINT Personen_FK FOREIGN KEY (fnr) REFERENCES `course-management`.funktion(fnr);
-
+INSERT INTO `course-management`.kurskontrolle
+(fnr, knr)
+VALUES
+(1,123),
+(2,123),
+(3,123),
+(4,123),
+(2,562),
+(3,562),
+(4,562),
+(3,234),
+(5,234),
+(3,341),
+(3,245),
+(4,245),
+(2,412),
+(3,412),
+(1,454),
+(2,454),
+(3,454),
+(4,454),
+(3,216),
+(3,456),
+(4,455),
+(4,345),
+(1,283),
+(2,283),
+(3,283),
+(1,776),
+(2,776),
+(5,245);
