48ab169f8b07b3def71391d84aa9a584b9cc8df1
Johannes Christeller <johannes.christeller@edu.teko.ch>
2020-08-18 18:14:51 +0200

    Tabellen person und funktion hinzugefügt.

--- Scripts/course-management.sql
+++ Scripts/course-management.sql
@@ -7,4 +7,15 @@ DROP SCHEMA IF EXISTS `course-management`;
 CREATE SCHEMA `course-management` DEFAULT CHARACTER SET utf8 ;
 USE `course-management`;
 
---
+--CREATE TABLE `person` (
+  `pnr` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `vorname` varchar(100) DEFAULT NULL,
+  `fnr` int DEFAULT NULL,
+  `lohnstufe` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `funktion` (
+  `fnr` int NOT NULL,
+  `funktion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
